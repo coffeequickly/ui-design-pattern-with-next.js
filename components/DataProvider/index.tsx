f6641cdb5983c2fd08a37f10ad7edfda8535e6fe
coffeequickly <jangwonseok.js@gmail.com>
2023-07-17 22:34:11 +0900

    [ADD] 데이터를 렌더 프롭스 형태로 넘겨줄 수 있는 컴포넌트 신규 생성

---
+++ components/DataProvider/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+interface DataProviderProps<T> {
+  queryFn: () => Promise<T>;
+  queryKey: (string | number)[];
+  render: (data: T) => React.ReactElement;
+}
+
+function DataProvider<T>({ queryFn, queryKey, render }: DataProviderProps<T>) {
+  const { data, isLoading, error } = useQuery<T, Error>(queryKey, queryFn);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    throw error;
+  }
+
+  return render(data!);
+}
+
+export default DataProvider;
